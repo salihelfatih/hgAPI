cb6bcc5b783aeb87b477d1cdcb07d63428b4e6b7
Salih <salihelobeid@gmail.com>
2022-12-03 21:23:10 -0500

    adds procFile

--- models/index.js
+++ models/index.js
@@ -11,6 +11,7 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
+  // console.log("config.use_env_variable", config.use_env_variable);
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
